7e01907bac8625100c5d8740719c9983e21194f2
ierofeev <ivanerofeev@bk.ru>
2023-01-15 18:01:02 +0400

    Add generate_diff method

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import json
 
 parser = argparse.ArgumentParser(
     prog='gendiff',
@@ -8,6 +9,48 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('-f', '--format', help='set format of output')
+parser.add_argument('first_file', help='path to the first file')
+parser.add_argument('second_file', help='path to the second file')
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+
+
+def dict_diff(first_dict: dict, second_dict: dict) -> dict:
+    result = {}
+
+    for key, value in first_dict.items():
+        if key in second_dict and value == second_dict[key]:
+            result[f'{str(key)}: {str(value)}'] = ' '
+        elif key in second_dict and value != second_dict[key]:
+            result[f'{str(key)}: {str(value)}'] = '-'
+            result[f'{str(key)}: {str(second_dict[key])}'] = '+'
+        else:
+            result[f'{str(key)}: {str(value)}'] = '-'
+
+    for key, value in second_dict.items():
+        if key not in first_dict:
+            result[f'{str(key)}: {str(value)}'] = '+'
+
+    return result
+
+
+def generate_diff(path_to_first_file: str, path_to_second_file: str) -> str:
+    first_file_data = json.load(open(path_to_first_file))
+    second_file_data = json.load(open(path_to_second_file))
+    result = ''
+
+    diff = dict_diff(first_file_data, second_file_data)
+    sorted_diff = sorted(diff)
+    for i in sorted_diff:
+        result += f'\n {diff[i]} {i}'
+    result = '{%s\n}' % result
+
+    return result
+
+
+def main():
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == '__main__':
+    main()
