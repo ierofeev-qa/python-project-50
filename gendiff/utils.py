ee6c1f6c3a5bfe3d16e1978c53463c3000ba9f48
ierofeev <ivanerofeev@bk.ru>
2023-02-18 15:24:08 +0400

    Add nested data structures support

--- gendiff/utils.py
+++ gendiff/utils.py
@@ -23,50 +23,88 @@ def to_lower(arg):
     return str(arg)
 
 
-def dict_diff(first_dict: dict, second_dict: dict) -> dict:
-    result = {}
+def to_primitive(arg):
+    a = '\'\"'
+    for i in a:
+        arg = str(arg).replace(i, '')
+    return arg
+
+
+def dict_diff(first_dict: dict, second_dict: dict) -> list:
+    result = []
 
     for key, value in first_dict.items():
+
+        if key in second_dict and \
+                type(value) is dict and \
+                type(second_dict[key]) is dict:
+            sign = ' '
+            result.append([str(key), dict_diff(value, second_dict[key]), sign])
+            continue
+
         if key not in second_dict or key in second_dict \
                 and second_dict[key] != value:
             sign = '-'
         else:
             sign = ' '
-        result[f'{str(key)}: {to_lower(value)}'] = sign
+        result.append([f'{str(key)}: {to_lower(value)}', sign])
 
     for key, value in second_dict.items():
         if key not in first_dict or key in first_dict \
-                and first_dict[key] != value:
+                and first_dict[key] != value and type(value) != dict:
             sign = '+'
         else:
-            sign = ' '
-        result[f'{str(key)}: {to_lower(value)}'] = sign
+            continue
+        result.append([f'{str(key)}: {to_lower(value)}', sign])
 
     return result
 
 
-def generate_diff(path_to_first_file: str, path_to_second_file: str) -> str:
-    yaml_extensions = ('.yml', 'yaml')
-    json_extensions = '.json'
+def parse_dict_data(value, replacer, spaces_count, counter):
     result = ''
+    sorted_value = sorted(value, key=lambda x: str(x[0]))
+
+    for item in sorted_value:
+        if type(item[1]) is list:
+            result += \
+                f'{replacer*spaces_count}{to_primitive(item[2])}' \
+                f' {to_primitive(item[0])}:' \
+                f' {{\n{parse_dict_data(item[1], replacer, spaces_count=spaces_count + counter, counter=counter)}' \
+                f'{replacer*spaces_count}}}\n'  # noqa: E501
+        else:
+            result += \
+                f'{replacer*spaces_count}{to_primitive(item[1])}' \
+                f' {to_primitive(item[0])}\n'
+
+    return result
+
+
+def stringify(value, replacer=' ', spaces_count=1):
+    if type(value) in (str, bool, int, float):
+        return to_primitive(value)
+    return '{\n' + parse_dict_data(value, replacer, spaces_count, spaces_count) + '}'  # noqa: E501
+
+
+def generate_diff(path_to_first_file: str, path_to_second_file: str) -> str:
+    yaml_extensions = ('.yml', '.yaml')
+    json_extensions = '.json'
 
     if path_to_first_file.endswith(yaml_extensions) \
             and path_to_second_file.endswith(yaml_extensions):
         first_file_data = yaml.safe_load(open(path_to_first_file))
         second_file_data = yaml.safe_load(open(path_to_second_file))
+
     elif path_to_first_file.endswith(json_extensions) \
             and path_to_second_file.endswith(json_extensions):
         first_file_data = json.load(open(path_to_first_file))
         second_file_data = json.load(open(path_to_second_file))
+
     else:
         raise AssertionError(
-            'Format is not supported. Supported format are JSON and YAML'
+            'Format is not supported. Supported formats are JSON and YAML'
         )
 
-    diff = dict_diff(first_file_data, second_file_data)
-    sorted_diff = sorted(diff)
-    for i in sorted_diff:
-        result += f'\n {diff[i]} {i}'
-    result = '{%s\n}' % result
+    diff_dict = dict_diff(first_file_data, second_file_data)
+    result = stringify(diff_dict)
 
     return result
